64b026ed4779e148d3e941c2c1cb4f931fd04c48
monicaschnei <monicaschnei@gmail.com>
2022-02-23 12:52:36 -0300

    adding some

--- src/components/institutional.js
+++ src/components/institutional.js
@@ -19,7 +19,7 @@ const Institutional = ({onClick}) => (
             <Row>
                 <Col xs={12} lg={5}></Col>
                 <Col xs={12} lg={7}>
-                    <h2 className='Institutional-title my-5'>Já nascemos digital</h2>
+                    <h2 className='Institutional-title my-5'>Já nascemos digita</h2>
                     <p className='mb-5'> is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
                     <IconText icon={faMobileAlt} color='#fff' className='mb-2'>Sem fila e sem burocracia</IconText>
                     <IconText icon={faMobile} color='#fff' className='mb-2'>Simples e prático</IconText>
