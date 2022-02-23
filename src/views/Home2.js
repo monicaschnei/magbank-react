699ff360d9e6e70e8af1e6312d95c8d89cb8429d
Fabricio de Sousa <41765675+bricio123@users.noreply.github.com>
2022-02-23 10:22:13 -0300

    adding more things about problems

--- src/views/Home2.js
+++ src/views/Home2.js
@@ -1,11 +1,6 @@
 import React, { useState } from "react";
 import Hero from "../components/Hero";
-import CreditCard from "../components/CreditCard";
 
-import CenteredButton from "../components/CenteredButton";
-import Institutional from "../components/institutional";
-import Faq2 from "../components/Faq2";
-import posts from "../data/posts";
 
 const Home2 = () => {
   <>
