485b943153637a937553bf7a5a96e91ab59cb3b8
Fabricio de Sousa <41765675+bricio123@users.noreply.github.com>
2022-02-22 23:46:11 -0300

    update

--- src/views/Home2.js
+++ src/views/Home2.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import Hero from "../components/Hero";
 import CreditCard from "../components/CreditCard";
-import CardList from "../components/CardList";
+
 import CenteredButton from "../components/CenteredButton";
 import Institutional from "../components/institutional";
 import Faq2 from "../components/Faq2";
